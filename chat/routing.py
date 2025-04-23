bb7e1d4aabae6dc43f023222bfca571ce326610a
chenna200 <chennakesavarao95@gmail.com>
2025-04-23 13:59:54 +0530

    first commit

---
+++ chat/routing.py
@@ -0,0 +1,7 @@
+from django.urls import path # type: ignore
+from . import consumers
+
+websocket_urlpatterns = [
+    path("ws/chat/<str:token>/<str:user_model>/<str:user_email>/", consumers.ChatConsumer.as_asgi()),
+    path('ws/message-notifications/<str:email>/', consumers.NotificationMessageConsumer.as_asgi()),
+]
